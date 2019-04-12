755cbb8c87226aac70364d32bd317e55aa9456ce
samuel200007 <48092905+samuel200007@users.noreply.github.com>
2019-04-12 10:28:12 +0200

    Update DockerWordpressBashScript.sh

--- DockerWordpressBashScript.sh
+++ DockerWordpressBashScript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Erstellt bei Samuel Briegel und Gian Burkhalter. 4.12.2019
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 mkdir my_wordpress
